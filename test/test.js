f0e5f2717448d9854157e10ac5783a42cd8bd33a
Mark Budos <markbudos@gmail.com>
2020-04-27 23:42:34 -0700

    working unit test

--- test/test.js
+++ test/test.js
@@ -1,35 +1,16 @@
 'use strict';
 
-/*
 var expect = require('chai').expect;
-var { numToWord, wordToNum } = require('../lib/index');
+var { CardsavrHelper } = require('../lib/cardsavr/CardsavrHelper');
 
-describe('#numFormatter', function() {
+describe('#CardsavrHelper', function() {
 
-    it('should convert one', function() {
-        var result = numToWord(1);
-        expect(result).to.equal('One');
-    });
-    it('should convert two', function() {
-        var result = numToWord(2);
-        expect(result).to.equal('Two');
-    });
-    it('should convert three', function() {
-        var result = numToWord(3);
-        expect(result).to.equal('Three');
-    });
-    it('should convert 1', function() {
-        var result = wordToNum('One');
-        expect(result).to.equal(1);
-    });
-    it('should convert 2', function() {
-        var result = wordToNum('Two');
-        expect(result).to.equal(2);
-    });
-    it('should convert 3', function() {
-        var result = wordToNum('Three');
-        expect(result).to.equal(3);
+    it('should initialize settings', function() {
+        var result = CardsavrHelper.getInstance().setAppSettings("api.localhost.cardsavr.com", "myapp", "mykey");
+        expect(result).instanceof(CardsavrHelper);
+        expect(result.app_name).equal("myapp");
+        expect(result.app_key).equal("mykey");
+        expect(result.cardsavr_server).equal("api.localhost.cardsavr.com");
     });
 
 });
-*/
