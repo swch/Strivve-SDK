2f04d7ba6bcf4989261c07465ba7a8fcd8f547f1
Mark Budos <markbudos@gmail.com>
2020-05-05 01:23:27 -0700

    Update webserver.js

--- sample/webserver.js
+++ sample/webserver.js
@@ -7,12 +7,9 @@ app.get('/create_user', function (req, res) {
     const app_name = "CardUpdatr Demo";
     const app_username = "cardupdatr_demo";
   
-    const app_key = "oqLSC5V/R4w42crQT7x0HjcM5NnS2tiyTzWSxnOVZdU=";
-    const app_password = "eyYhGC#n!89r";
+    const app_key = "[REDACTED]";
+    const app_password = "[REDACTED]";
     const cardsavr_server = "https://api.localhost.cardsavr.io";
-    //const app_key = "TGSEjt4TuK0j55TeF7x1cao88Bc1j8nyHeaBHueT5gQ=";
-    //const app_password = "uLa64$#Rf8bh";
-    //const cardsavr_server = "https://api.mbudos.cardsavr.io";
   
     const cardholder_data = require("./cardholder.json");
     const address_data = require("./address.json");
@@ -27,7 +24,6 @@ app.get('/create_user', function (req, res) {
             res.redirect("/#" + queryString);
         }).catch(err => console.log(err));
 
-        
 })
 
 app.use(express.static('../dist'))
