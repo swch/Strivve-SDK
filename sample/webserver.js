58f8674c8d2fdd185ab9575c9f1b98a9f6497ba7
Mark Budos <markbudos@gmail.com>
2020-05-18 12:29:41 -0700

    fixed

--- sample/webserver.js
+++ sample/webserver.js
@@ -5,7 +5,7 @@ const { CardsavrHelper } = require("@strivve/strivve-sdk/lib/cardsavr/CardsavrHe
 
 app.get('/create_user', function (req, res) {
  
-    const {app_name, app_key, app_username, app_password, cardsavr_server } = require("./strivve_creds_mark.json");
+    const {app_name, app_key, app_username, app_password, cardsavr_server } = require("./strivve_creds.json");
     const cardholder_data = require("./cardholder.json");
     const address_data = require("./address.json");
     const card_data = require("./card.json");
@@ -32,4 +32,4 @@ app.get('/create_user', function (req, res) {
 
 app.use(express.static('../dist'))
 
-app.listen(port, () => console.log(`Example app listening at http://static.mbudos.cardsarv.io:${port}`))
+app.listen(port, () => console.log(`Example app listening at ${port}`))
