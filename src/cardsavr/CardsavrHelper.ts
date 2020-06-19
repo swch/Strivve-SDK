3632d69a0415aee6554e0d62c10a6e37541f4143
Mark Budos <markbudos@gmail.com>
2020-06-18 20:56:58 -0700

    Update CardsavrHelper.ts

--- src/cardsavr/CardsavrHelper.ts
+++ src/cardsavr/CardsavrHelper.ts
@@ -47,7 +47,6 @@ export class CardsavrHelper {
             const session = new CardsavrSession(this.cardsavr_server, this.app_key, this.app_name, username, password, grant, this.cert, trace);
             const login_data = await session.init();
             this.sessions[username] = { session: session, user_id: login_data.body.user_id, cardholder_safe_key: login_data.body.cardholder_safe_key, account_map: {} }; 
-            console.log(this.sessions[username]);
             return this.sessions[username];
         } catch(err) {
             this.handleError(err);
