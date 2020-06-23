8d583df7efe7b282ee870241e2ee336dfd7c60ac
Mark Budos <markbudos@gmail.com>
2020-06-23 07:17:19 -0700

    Update webserver.js

--- sample/webserver.js
+++ sample/webserver.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const { CardsavrHelper } = require("@strivve/strivve-sdk/lib/cardsavr/CardsavrHelper");
 
 app.get('/create_user', function (req, res) {
