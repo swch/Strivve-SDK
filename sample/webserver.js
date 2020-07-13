bde4af4eafcdf691534f51a2eb684c0f1d509185
Mark Budos <markbudos@gmail.com>
2020-07-13 12:49:49 -0700

    Update webserver.js

--- sample/webserver.js
+++ sample/webserver.js
@@ -32,6 +32,7 @@ app.get("/create_user", function (req, res) {
 });
 
 const static_dir = process.env.npm_package_config_static_dir || "../dist";
+console.log("USING " + static_dir + " for static file serving.");
 app.use(express.static(static_dir));
 
 app.listen(port, () => console.log(`CardUpdatr Demo app listening at ${port}`));
